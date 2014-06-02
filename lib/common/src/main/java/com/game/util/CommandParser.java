91ac5213db4600cb025e454f1ee9d09a7bc083b7
Bình Minh <nguyendinhminh27092gmail.com>
2014-06-02 17:10:31 +0700

    add Module Library

---
+++ lib/common/src/main/java/com/game/util/CommandParser.java
@@ -0,0 +1,16 @@
+
+package com.game.util;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+
+public class CommandParser {
+
+  /**org.apache.commons.cli.Options*/
+  private Options options;
+  
+  /**org.apache.commons.cli.CommandLine*/
+  private CommandLine cmd;
+  
+  private String helpMesg;
+}
